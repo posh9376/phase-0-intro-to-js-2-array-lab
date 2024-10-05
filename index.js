9b01a10bde020c531826e60a1a99666a10f76534
Eric Richard <reric0802@gmail.com>
2024-10-05 18:00:17 +0300

    added code to ensure passing

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution here!
+const cats = ['Milo', 'Otis', 'Garfield']
+
+function destructivelyAppendCat(name) {
+    return cats.push('Ralph')
+}
+function destructivelyPrependCat(name) {
+    return cats.unshift('Bob')
+}
+
+function destructivelyRemoveLastCat(name) {
+    return cats.pop()
+}
+function destructivelyRemoveFirstCat() {
+    return cats.shift()
+}
+function appendCat(name) {
+    return [...cats, 'Broom']
+}
+function prependCat(name) {
+    return['Arnold',...cats]
+}
+
+function removeLastCat(params) {
+    return [...cats.slice(0,2)]
+}
+
+function removeFirstCat() {
+    return [...cats.slice(1)]
+}
